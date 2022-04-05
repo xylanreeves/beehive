b4fd54f98fcfc64b77f5f42bfaf36b8829a18045
xylan <38619296+xylanreeves@users.noreply.github.com>
2022-04-05 21:51:22 +0530

    Created Raffle.sol
    contract for buying tokens

---
+++ Raffle.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Raffle is ERC20, ReentrancyGuard{
+
+
+
+// TODO
+function buyRaffleTokens() public payable {
+    //send mtr to get raffle native tokens
+    //1mtr = 1000 rfl
+    
+}
+
+
+
+
+
+}
